d4992d5ae2ca0928c88645c2c2ea375f73af9199
Vansh Vasishtha <vansh19092002@gmail.com>
2024-05-07 14:55:29 +0530

    Routing sorted For pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,15 +1,29 @@
+"use client";
 import WebSignup from "@/components/form";
-import exp from "constants";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/navigation";
+import { auth } from "../../../firebase-config";
 
 type Props = {};
 
 const Login = (props: Props) => {
-  return (
-    <div>
-      <WebSignup />
-    </div>
-  );
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+
+  if (loading) {
+    console.log("Loading...");
+    return <div>Loading...</div>
+  } else {
+    if (!user) {
+      return (
+       <WebSignup />
+      );
+    } else {
+      router.replace("/");
+      return <div>Loading...</div>
+    }
+  }
 };
 
 export default Login;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+"use client"
 import WebHome from "@/components/home";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase-config";
 
 export default function Home() {
-  return (
-   <WebHome />
-  );
+  const [user, loading] = useAuthState(auth);
+
+  const router = useRouter();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (user) {
+    return <WebHome />;
+  } else {
+    router.replace("/login");
+  }
 }
